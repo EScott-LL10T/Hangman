449a9f48fed2b443c091d0710dff886836a36872
Elliot Scott <elliotscott2111@gmail.com>
2026-06-09 00:00:47 +0100

    Started writing the hangman() method that is called by main and wrote method setWordState that adds _ to every element of the list at the start for the number of letters in word.

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,3 +1,27 @@
-public class Hangman {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Hangman {
+    public static void main(String[] args){
+        hangman();
+    }
+
+    public static void hangman(){
+        String word = "pizza";
+
+        Scanner s = new Scanner(System.in);
+        ArrayList<Character> wordState = new ArrayList<>();
+        setWordState(wordState, word.length());
+        int wrongGuesses = 0;
+
+        IO.println(wordState.toString());
+
+        s.close();
+    }
+
+    public static void setWordState(ArrayList<Character> wordState, int wordLength){
+        for(int i = 0; i < wordLength; i++){
+            wordState.add('_');
+        }
+    }
 }
